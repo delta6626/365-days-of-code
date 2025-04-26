f48a1952822cfc329cede7070b5ba60582459428
delta911ee <hassan911ee@gmail.com>
2025-04-26 19:46:43 +0400

    chore: Configured routing

--- day9-dayx/nebula/src/App.jsx
+++ day9-dayx/nebula/src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./features/core/HomePage";
+import DashboardPage from "./features/core/DashboardPage";
+import PricingPage from "./features/core/PricingPage";
+import SignUpPage from "./features/auth/SignUpPage";
+import LogInPage from "./features/auth/LogInPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route
+          path="/dashboard"
+          element={<DashboardPage></DashboardPage>}
+        ></Route>
+        <Route path="/pricing" element={<PricingPage></PricingPage>}></Route>
+        <Route path="/signup" element={<SignUpPage></SignUpPage>}></Route>
+        <Route path="/login" element={<LogInPage></LogInPage>}></Route>
       </Routes>
     </Router>
   );
