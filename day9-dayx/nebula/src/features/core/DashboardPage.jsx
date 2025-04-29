7b71dc76f19003a8c33197eee00be083e2f71853
Delta6626 <hassan911ee@gmail.com>
2025-04-29 11:28:48 +0400

    feat: designed sidebar for dashboard page

--- day9-dayx/nebula/src/features/core/DashboardPage.jsx
+++ day9-dayx/nebula/src/features/core/DashboardPage.jsx
@@ -1,5 +1,41 @@
+import { ArrowLeft, File, LayoutGrid, Notebook, Settings } from "lucide-react";
+
 function DashboardPage() {
-  return;
+  return (
+    <div className="">
+      <div className="sideBar w-60 h-[100vh] bg-base-300 p-4 flex flex-col">
+        <div className="logo flex items-center justify-between">
+          <h1 className="text-xl font-jakarta font-bold text-center">Nebula</h1>
+          <button className="btn rounded-lg">
+            <ArrowLeft></ArrowLeft>
+          </button>
+        </div>
+        <div className="divider"></div>
+        <div className="mainButtons flex-1">
+          <button className="btn btn-wide justify-start rounded-lg">
+            <LayoutGrid></LayoutGrid>
+            <p>Dashboard</p>
+          </button>
+
+          <button className="btn btn-wide justify-start rounded-lg mt-2">
+            <File></File>
+            <p>Notes</p>
+          </button>
+
+          <button className="btn btn-wide justify-start rounded-lg mt-2">
+            <Notebook></Notebook>
+            <p>Notebook</p>
+          </button>
+        </div>
+        <div className="bottomButtons">
+          <button className="btn btn-wide justify-start rounded-lg mt-2">
+            <Settings></Settings>
+            <p>Settings</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardPage;
