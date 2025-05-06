fb6d4f689ca87ffa71db43cf95ca279d2e81e79e
Delta6626 <hassan911ee@gmail.com>
2025-05-06 18:01:05 +0400

    feat: show pinned notes count  in dashboard

--- day9-dayx/nebula/src/features/core/DashboardArea.jsx
+++ day9-dayx/nebula/src/features/core/DashboardArea.jsx
@@ -4,10 +4,18 @@ import GridNote from "../components/GridNote";
 import GenericModal from "../components/GenericModal";
 import EditNoteModal from "../components/EditNoteModal";
 import { APP_CONSTANTS } from "../../constants/APP_CONSTANTS";
+import { useState } from "react";
+import { Search } from "lucide-react";
 
 function DashboardArea() {
   const { notes, setNotes } = useNotesStore();
   const { message, setMessage } = useMessageStore();
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const pinnedNotes = notes.filter((note) => note.pinned === true);
+
+  function handleSearch() {}
 
   return (
     <div className="flex-1 h-[100vh] p-4 font-jakarta overflow-y-scroll scroll-smooth scrollbar-thin">
@@ -25,14 +33,33 @@ function DashboardArea() {
       <EditNoteModal></EditNoteModal>
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Dashboard</h1>
+        <div className="flex">
+          <div className="w-2xl input focus-within:input-primary">
+            <Search className="text-gray-400"></Search>
+            <input
+              className=""
+              placeholder="Search anything"
+              type="text"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+          </div>
+          <button className="btn btn-primary ml-2">Create</button>
+        </div>
+        <div className="">
+          <h1></h1>
+        </div>
       </div>
       <div className="divider"></div>
-      <div className="flex gap-5 flex-wrap">
-        {notes.map((note, id) => {
-          if (note.pinned == true) {
-            return <GridNote key={id} noteObject={note}></GridNote>;
-          }
-        })}
+      <div className="">
+        <h3 className="text-xl font-semibold">
+          Pinned notes ({pinnedNotes.length})
+        </h3>
+        <div className="flex gap-5 flex-wrap mt-4">
+          {pinnedNotes.map((note, id) => (
+            <GridNote key={id} noteObject={note} />
+          ))}
+        </div>
       </div>
     </div>
   );
