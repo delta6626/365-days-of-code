4eafc9883f025698a7b61e5e0754d83be05f9805
Delta6626 <hassan911ee@gmail.com>
2025-05-23 12:48:08 +0400

    feat: more data for user stats

--- day9-dayx/nebula/src/features/components/UserStatistics.jsx
+++ day9-dayx/nebula/src/features/components/UserStatistics.jsx
@@ -6,25 +6,59 @@ function UserStatistics() {
   const { notes } = useNotesStore();
   const { notebooks } = useNotebooksStore();
 
+  let pinnedNotesCount = 0;
+  let pinnedNotebooksCount = 0;
+  let allTags = [];
+  let uniqueTags;
+
+  notes.forEach((note) => {
+    if (note.pinned) {
+      pinnedNotesCount += 1;
+    }
+
+    allTags = [...allTags, ...note.tags];
+  });
+
+  notebooks.forEach((notebook) => {
+    if (notebook.pinned) {
+      pinnedNotebooksCount += 1;
+    }
+
+    allTags = [...allTags, ...notebook.tags];
+  });
+
+  uniqueTags = new Set(allTags);
+
   return (
     <div className="bg-transparent border-1 border-base-200 mt-4 w-sm mx-auto rounded-lg p-4">
       <h1 className="flex items-center gap-2 font-semibold text-gray-400">
         <ActivitySquare />
         Your stats
       </h1>
-      <div className="">
-        <h1 className="flex items-center gap-2 mt-4">
-          <span>•</span>{" "}
-          {notebooks.length === 1
-            ? "1 notebook in total"
-            : `${notebooks.length} notebooks in total`}
-        </h1>
-        <h1 className="flex items-center gap-2 mt-2">
-          <span>•</span>{" "}
-          {notes.length === 1
-            ? "1 note in total"
-            : `${notes.length} notes in total`}
-        </h1>
+      <div className="mt-4 px-4">
+        <ul className="list-disc">
+          <li>
+            {`${notebooks.length} ${
+              notebooks.length == 1 ? "notebook" : "notebooks"
+            }`}
+          </li>
+          <li className="mt-2">{`${notes.length} ${
+            notes.length == 1 ? "note" : "notes"
+          }`}</li>
+          <li className="mt-2">
+            {`${pinnedNotebooksCount} ${
+              pinnedNotebooksCount == 1 ? "pinned notebook" : "pinned notebooks"
+            }`}
+          </li>
+          <li className="mt-2">
+            {`${pinnedNotesCount} ${
+              pinnedNotesCount == 1 ? "pinned note" : "pinned notes"
+            }`}
+          </li>
+          <li className="mt-2">{`${uniqueTags.size} unique ${
+            uniqueTags.size == 1 ? "tag" : "tags"
+          }`}</li>
+        </ul>
       </div>
     </div>
   );
