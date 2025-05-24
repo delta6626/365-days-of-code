b7045a40067fe2b63da617f4c1738d0cfd2e5c9b
Delta6626 <hassan911ee@gmail.com>
2025-05-24 17:38:22 +0400

    feat: show tooltip if email not verified

--- day9-dayx/nebula/src/features/components/QuickActions.jsx
+++ day9-dayx/nebula/src/features/components/QuickActions.jsx
@@ -22,33 +22,42 @@ function QuickActions() {
 
   return (
     <div className="flex gap-5 mt-4 w-full">
-      <button
-        className="btn w-sm h-[10rem] flex flex-col"
-        disabled={!userVerified}
-        onClick={handleNewNoteClick}
+      <div
+        className={!userVerified ? "tooltip tooltip-bottom" : ""}
+        data-tip={APP_CONSTANTS.VERIFY_EMAIL}
       >
-        <FilePlus size={30} />
-        <p className="text-xl">New note</p>
-        <div className="flex flex-row items-center text-gray-400">
-          {user ? `Shift + ${user.shortcuts.NEW_NOTE}` : ""}
-        </div>
-      </button>
+        <button
+          className="btn w-sm h-[10rem] flex flex-col"
+          disabled={!userVerified}
+          onClick={handleNewNoteClick}
+        >
+          <FilePlus size={30} />
+          <p className="text-xl">New note</p>
+          <div className="flex flex-row items-center text-gray-400">
+            {user ? `Shift + ${user.shortcuts.NEW_NOTE}` : ""}
+          </div>
+        </button>
+      </div>
+
+      <div
+        className={!userVerified ? "tooltip tooltip-bottom" : ""}
+        data-tip={APP_CONSTANTS.VERIFY_EMAIL}
+      >
+        <button
+          className="btn w-sm h-[10rem] flex flex-col"
+          disabled={!userVerified}
+          onClick={handleNewNotebookClick}
+        >
+          <BookPlus size={30} />
+          <p className="text-xl">New notebook</p>
+          <div className="flex flex-row items-center text-gray-400">
+            {user ? `Shift + ${user.shortcuts.NEW_NOTE_BOOK}` : ""}
+          </div>
+        </button>
+      </div>
 
       <button
         className="btn w-sm h-[10rem] flex flex-col"
-        disabled={!userVerified}
-        onClick={handleNewNotebookClick}
-      >
-        <BookPlus size={30} />
-        <p className="text-xl">New notebook</p>
-        <div className="flex flex-row items-center text-gray-400">
-          {user ? `Shift + ${user.shortcuts.NEW_NOTE_BOOK}` : ""}
-        </div>
-      </button>
-
-      <button
-        className="btn w-sm h-[10rem] flex flex-col"
-        disabled={!userVerified}
         onClick={handleRecentClick}
       >
         <Clock size={30} />
