d25215e84ef9965a783dc717ae634f3f4342fc56
delta911ee <hassan911ee@gmail.com>
2025-05-30 23:20:28 +0400

    terms of service title

--- day9-dayx/nebula/src/features/misc/TermsOfService.jsx
+++ day9-dayx/nebula/src/features/misc/TermsOfService.jsx
@@ -4,6 +4,9 @@ function TermsOfService() {
   return (
     <div className="">
       <NavBar />
+      <div className="w-full px-60 py-10 font-jakarta text-center">
+        <h1 className="text-4xl font-semibold">Terms of Service</h1>
+      </div>
     </div>
   );
 }
