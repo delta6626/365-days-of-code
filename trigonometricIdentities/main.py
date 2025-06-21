b6fbc6c319bc91b27c4d66cf0a0381113cac9e1e
Delta6626 <hassan911ee@gmail.com>
2025-06-21 23:34:48 +0400

    draw triangle

--- trigonometricIdentities/main.py
+++ trigonometricIdentities/main.py
@@ -6,3 +6,18 @@
 # 1 + cot^2(x) = cosec^2(x)
 
 from manim import *
+
+class Animation(Scene):
+    def construct(self):
+
+        # Vertices of the right triangle
+        A = ORIGIN                  # Point at (0,0)
+        B = UP * 2                 # Point at (0,2)
+        C = RIGHT * 2              # Point at (2,0)
+
+        # Create the right triangle
+        rightTriangle = Polygon(A, B, C)
+        rightAngle = RightAngle(Line(A, B), Line(A, C))
+
+        self.play(Create(rightTriangle))
+        self.play(Create(rightAngle))
