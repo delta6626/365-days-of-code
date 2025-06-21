beb65cd3acbd3e546e49a566fa5627857c74c7c9
Delta6626 <hassan911ee@gmail.com>
2025-06-21 22:57:40 +0400

    initial commit

---
+++ trigonometricIdentities/main.py
@@ -0,0 +1,8 @@
+# A Manim script to animate the derivation of three key trigonometric identities:
+# Author: https://github.com/delta6626
+
+# sin^2(x) + cos^2(x) = 1
+# 1 + tan^2(x) = sec^2(x)
+# 1 + cot^2(x) = cosec^2(x)
+
+from manim import *
